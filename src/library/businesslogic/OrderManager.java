2f10316528a06a76b2573330912efc9d392e0630
Ruben Hamelink <hoi@hoi.hoi>
2016-04-05 20:59:16 +0200

    OrderManager
    Voorbeeld gecomment

--- src/library/businesslogic/OrderManager.java
+++ src/library/businesslogic/OrderManager.java
@@ -26,10 +26,11 @@ public class OrderManager {
         
         fillTestData();
         
+        /* Voorbeeld van een order
         Order order = placeOrder(tables.get(1), new Dish(1, "Ei met spek", 10), new Drink(1, "Cola", 2));
         order.setDishAmount(1);
         order.setDrinkAmount(1);
-        order.writeToDatabase();
+        order.writeToDatabase();*/
     }
 
     private void fillTestData()
